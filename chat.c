29572c7880d4f3eeebb9310d02f382694beb0f2f
michalovsk <92398112+michalovsk@users.noreply.github.com>
2022-01-08 12:30:56 +0100

    fixnutie písania, parsovanie prázdneho stringu pri posielaní, pridanie enteru pre posielanie správy.

--- chat.c
+++ chat.c
@@ -6,45 +6,54 @@ GtkTextMark *mark;
 
 
 void on_buttonSendMessage_clicked(GtkButton *button, gpointer user_data) {
+    const gchar *textInsert;
     chatTextBuffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(gtkViewText));
-    //textInsert = gtk_entry_get_text(GTK_ENTRY(gtkSendText));
-    textInsert = gtk_text_view_get_buffer(GTK_TEXT_VIEW(gtkSendText));
+    textInsert = gtk_entry_get_text(GTK_ENTRY(gtkSendText));
+
     printf("sent!\n");
-    const gchar* fixedText;
-    g_utf8_validate((const gchar*)textInsert, 50, &fixedText);
-
-
     mark = gtk_text_buffer_get_insert (chatTextBuffer);
     gtk_text_buffer_get_iter_at_mark (chatTextBuffer, &iter, mark);
-    if (gtk_text_buffer_get_char_count(chatTextBuffer))
+    //if (gtk_text_buffer_get_char_count(chatTextBuffer) > 2)
+    //    gtk_text_buffer_insert (chatTextBuffer, &iter, "\n", 1);
+    //gtk_text_buffer_insert (chatTextBuffer, &iter, textInsert, -1);
+    if (strlen(textInsert) > 0) {
+        printf("Nice.");
         gtk_text_buffer_insert (chatTextBuffer, &iter, "\n", 1);
-    gtk_text_buffer_insert (chatTextBuffer, &iter, fixedText, -1);
-
-
+        gtk_text_buffer_insert (chatTextBuffer, &iter, textInsert, -1);
+    } else {
+        printf("Not nice.");
+    }
     gtk_text_view_scroll_to_mark(GTK_TEXT_VIEW(gtkViewText), mark, 0.0, FALSE, 1, 1);
-
+    gtk_entry_set_text(GTK_ENTRY(gtkSendText), "");
 
 }
 
 void on_buttonReceiveMessage_clicked(GtkButton *button, gpointer user_data) { // TODO DELETE, JUST FOR TESTING!!!!!
 
-   /* const gchar *textInsert;
-
-    printf("received!\n");
+    const gchar *textInsert;
     chatTextBuffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(gtkViewText));
     textInsert = gtk_entry_get_text(GTK_ENTRY(gtkSendText));
-    //textInsert = gtk_text_view_get_buffer(GTK_TEXT_VIEW(gtkSendText));
 
+
+    printf("received!\n");
     mark = gtk_text_buffer_get_insert (chatTextBuffer);
-    //gtk_text_buffer_get_mark(chatTextBuffer, (const gchar*)mark2);
-    //gtk_text_buffer_get_iter_at_mark (chatTextBuffer, &iter, mark);
-    if (gtk_text_buffer_get_char_count(chatTextBuffer))
+    gtk_text_buffer_get_iter_at_mark (chatTextBuffer, &iter, mark);
+    gtk_text_buffer_create_mark(chatTextBuffer, "rightToLeft", &iter, FALSE);
+    if (strlen(textInsert) > 0) {
+        printf("Nice.");
         gtk_text_buffer_insert (chatTextBuffer, &iter, "\n", 1);
-    gtk_text_buffer_insert (chatTextBuffer, &iter, textInsert, -1);
-    //gtk_text_view_scroll_to_mark(GTK_TEXT_VIEW(gtkViewText), mark, 0, 0, 1, 1);
-    gtk_text_view_scroll_to_mark(GTK_TEXT_VIEW(gtkViewText), mark, 0.0, FALSE, 1, 1);*/
+        gtk_text_buffer_insert (chatTextBuffer, &iter, textInsert, -1);
+    } else {
+        printf("Not nice.");
+    }
+
+
+
+    gtk_text_view_scroll_to_mark(GTK_TEXT_VIEW(gtkViewText), mark, 0.0, FALSE, 1, 1);
+    gtk_entry_set_text(GTK_ENTRY(gtkSendText), "");
 }
 void chatScreen() {
+
     builder = gtk_builder_new_from_file("chat.glade");
     gtk_builder_connect_signals(builder, NULL);
 
@@ -52,6 +61,7 @@ void chatScreen() {
     gtkViewText = GTK_WIDGET(gtk_builder_get_object(builder, "gtkViewText"));
     gtkSendText = GTK_WIDGET(gtk_builder_get_object(builder, "gtkSendText"));
     buttonSendMessage = GTK_WIDGET(gtk_builder_get_object(builder, "buttonSendMessage"));
+    buttonReceiveMessage = GTK_WIDGET(gtk_builder_get_object(builder, "buttonReceiveMessage"));
 
     gtk_window_set_default_size(GTK_WINDOW(windowChat),600,450);
     gtk_text_view_set_editable(GTK_TEXT_VIEW(gtkViewText), FALSE);
@@ -59,7 +69,8 @@ void chatScreen() {
     // TODO pridať emoji
     g_signal_connect(windowChat, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(buttonSendMessage, "clicked", G_CALLBACK(on_buttonSendMessage_clicked), windowChat);
-    g_signal_connect(buttonReceiveMessage, "clicked2", G_CALLBACK(on_buttonReceiveMessage_clicked), windowChat);
+    g_signal_connect(buttonReceiveMessage, "clicked", G_CALLBACK(on_buttonReceiveMessage_clicked), windowChat);
+    g_signal_connect(gtkSendText, "activate", G_CALLBACK(on_buttonSendMessage_clicked), NULL);
     gtk_widget_show_all(windowChat);
     gtk_main();
 
